0b0ed874ffced694480f66a909d5858ca65e979f
Philip Huang <philip.sw.huang@gmail.com>
2020-12-07 11:50:02 -0800

    Day 7 Solution

--- day7HandyHaversacks.js
+++ day7HandyHaversacks.js
@@ -28,3 +28,63 @@
 
 // How many bag colors can eventually contain at least one shiny gold bag? (The list of rules is quite long; make sure you get all of it.)
 
+var fs = require('fs')
+var input = fs.readFileSync("./day7Input.txt").toString().split('\r\n')
+// console.log(input[0])
+
+// var test = ["light red bags contain 1 bright white bag, 2 muted yellow bags.",
+//             "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+//             "bright white bags contain 1 shiny gold bag.",
+//             "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+//             "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+//             "dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+//             "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+//             "faded blue bags contain no other bags."]
+
+var containsColorSet = new Set()
+// this finds all the colored bags that it can contain and adds it to the set
+// i.e. bags === input or test
+// color === 'shiny gold'
+// calculateBags(input, 'shiny gold') returns nothing, but each colored bag that can contain 'shiny gold' bags will get added to the set
+// containsColorSet = { 'dull fuchsia',
+//                      'clear indigo',
+//                      'mirrored lavender',
+//                      'wavy yellow',
+//                      'muted aqua',
+//                      'light tomato',
+//                      'striped maroon',
+//                      'dotted beige',
+//                      'dim chartreuse'
+// }
+const calculateBags = (bags, color) => {
+  for (let i = 0; i < bags.length; i++) {
+    if (bags[i].match(color)) {
+      if (bags[i].slice(0, color.length) !== color) {
+        let idx = bags[i].indexOf('bags')
+        let bagColor = bags[i].slice(0, idx-1)
+        containsColorSet.add(bagColor)
+      }
+    }
+  }
+  console.log(containsColorSet)
+}
+calculateBags(input, 'shiny gold')
+calculateBags(input, 'dull fuchsia')
+
+// This calculates EACH element in the Set and continually adds colors that can contain that color bag
+// i.e. 'dull fuchsia' will add to the set { 'posh silver', 'mirrored white', 'vibrant tan', 'bright chartreuse' }
+// This continues until it has found ALL bags that can contain 'shiny gold' bags --> ALL elements in the Set containsColorSet
+const calculateContains = (bags) => {
+  bags.forEach(color => calculateBags(input, color))
+  console.log(bags)
+  return bags.size
+}
+
+console.log(calculateContains(containsColorSet))
+
+
+
+// find all occurrences of 'shiny gold' store into a Sett
+// take that Set, get all 'colors' that don't start with 'shiny gold' and put those colors into Set
+// iterate through the Set and find other bags that can contain those colors
+// log the size of the Set once all elements have been iterated through

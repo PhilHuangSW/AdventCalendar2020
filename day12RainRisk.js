a14fa190e2e05a92c6cb13ec81d3f10017c4c980
Philip Huang <philip.sw.huang@gmail.com>
2020-12-14 00:34:46 -0800

    Day 12 Solution

--- day12RainRisk.js
+++ day12RainRisk.js
@@ -32,3 +32,182 @@
 
 // Figure out where the navigation instructions lead. What is the Manhattan distance between that location and the ship's starting position?
 
+var fs = require('fs')
+var input = fs.readFileSync("./day12Input.txt").toString().split("\r\n")
+var test = ['F10','N3','F7','R90','F11']
+
+const datify = (array) => {
+  let dataArray = []
+  for (let i = 0; i < array.length; i++) {
+    let direction = []
+    direction.push(array[i][0])
+    direction.push(parseInt(array[i].slice(1,array[i].length)))
+    dataArray.push(direction)
+  }
+  return dataArray
+} 
+
+test = datify(test)
+data = datify(input)
+
+const manhattanDistance = (directions) => {
+  let compass = ['N', 'E', 'S', 'W']
+  let facing = 1
+  let east = 0
+  let west = 0
+  let north = 0
+  let south = 0
+  for (let i = 0; i < directions.length; i++) {
+    switch(directions[i][0]) {
+      case 'E':
+        if (west === 0) {
+          east += directions[i][1]
+        } else {
+          let eastDiff = west - directions[i][1]
+          if (eastDiff < 0) {
+            east += Math.abs(eastDiff)
+            west = 0
+          } else if (eastDiff === 0) {
+            west = 0
+          } else {
+            west = eastDiff
+          }
+        }
+        break;
+      case 'W':
+        if (east === 0) {
+          west += directions[i][1]
+        } else {
+          let westDiff = east - directions[i][1]
+          if (westDiff < 0) {
+            west += Math.abs(westDiff)
+            east = 0
+          } else if (westDiff === 0) {
+            east = 0
+          } else {
+            east = westDiff
+          }
+        }
+        break;
+      case 'N':
+        if (south === 0) {
+          north += directions[i][1]
+        } else {
+          let northDiff = south - directions[i][1]
+          if (northDiff < 0) {
+            north += Math.abs(northDiff)
+            south = 0
+          } else if (northDiff === 0) {
+            south = 0
+          } else {
+            south = northDiff
+          }
+        }
+        break;
+      case 'S':
+        if (north === 0) {
+          south += directions[i][1]
+        } else {
+          let southDiff = north - directions[i][1]
+          if (southDiff < 0) {
+            south += Math.abs(southDiff)
+            north = 0
+          } else if (southDiff === 0) {
+            north = 0
+          } else {
+            north = southDiff
+          }
+        }
+        break;
+      case 'R':
+        let degreesRight = (directions[i][1]%360)
+        if (degreesRight === 90) {
+          facing += 1
+          facing %= 4
+        } else if (degreesRight === 180) {
+          facing += 2
+          facing %= 4
+        } else if (degreesRight === 270) {
+          facing += 3
+          facing %= 4
+        }
+        break;
+      case 'L':
+        let degreesLeft = (directions[i][1]%360)
+        if (degreesLeft === 90) {
+          facing += 3
+          facing %= 4
+        } else if (degreesLeft === 180) {
+          facing += 2
+          facing %= 4
+        } else if (degreesLeft === 270) {
+          facing += 1
+          facing %= 4
+        }
+        break;
+      default:
+        if (compass[facing] === 'N') {
+          if (south === 0) {
+            north += directions[i][1]
+          } else {
+            let northDiff = south - directions[i][1]
+            if (northDiff < 0) {
+              north += Math.abs(northDiff)
+              south = 0
+            } else if (northDiff === 0) {
+              south = 0
+            } else {
+              south = northDiff
+            }
+          }
+        } else if (compass[facing] === 'E') {
+          if (west === 0) {
+            east += directions[i][1]
+          } else {
+            let eastDiff = west - directions[i][1]
+            if (eastDiff < 0) {
+              east += Math.abs(eastDiff)
+              west = 0
+            } else if (eastDiff === 0) {
+              west = 0
+            } else {
+              west = eastDiff
+            }
+          }
+        } else if (compass[facing] === 'S') {
+          if (north === 0) {
+            south += directions[i][1]
+          } else {
+            let southDiff = north - directions[i][1]
+            if (southDiff < 0) {
+              south += Math.abs(southDiff)
+              north = 0
+            } else if (southDiff === 0) {
+              north = 0
+            } else {
+              north = southDiff
+            }
+          }
+        } else {
+          if (east === 0) {
+            west += directions[i][1]
+          } else {
+            let westDiff = east - directions[i][1]
+            if (westDiff < 0) {
+              west += Math.abs(westDiff)
+              east = 0
+            } else if (westDiff === 0) {
+              east = 0
+            } else {
+              east = westDiff
+            }
+          }
+        }
+        break;
+    }
+    console.log(`north: ${north} -- east: ${east} -- south: ${south} -- west: ${west}`)
+  }
+  return (north + east + south + west)
+}
+
+console.log(manhattanDistance(data))  // 1645 --> Correct Answer!
